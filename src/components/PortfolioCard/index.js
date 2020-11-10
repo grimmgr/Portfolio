223226310a48f42ba83ab24a39342df1b9643d46
grimmgr <ggrimm33@gmail.com>
2020-11-09 17:29:20 -0800

    re-size cards

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -5,9 +5,9 @@ export const PortfolioCard = (props) => {
     const { id, name, link, image, description } = props;
     return (
         <div className='card-wrapper'>
-            <div id={id} className='card light'>
-                <div className='title-wrapper dark'>
-                  <a href={link} target='_blank' rel='noreferrer'>{name}</a>
+            <div id={id} className='card'>
+                <div className='title-wrapper'>
+                  <a href={link} target='_blank' rel='noreferrer'><h3>{name}</h3></a>
                 </div>
                 <div className='img-wrapper'>
                   <img src={image} alt={name} />
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ export const Contact = () => {
     return (
         <section id='contact'>
             <p>email me..</p>
-            <p className='emoji'>....〆(・∀・)</p>
+            <p className='emoji'>..〆(・∀・)</p>
             <p><a href='mailto: ggrimm33@gmail.com' target='_blank' rel='noreferrer'>ggrimm33@gmail. com</a></p>
         </section>
     )
