acd2d425bbe9db81e41f619e94c541e65414d9e8
grimmgr <ggrimm33@gmail.com>
2021-01-26 12:15:41 -0800

    card fronts

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -7,22 +7,28 @@ export const PortfolioCard = (props) => {
 
             <div id={`card-wrapper${id}`} className='card-wrapper'>
                 <div id={`card${id}`} className='card'>
-                    <div className='title-wrapper'>
-                        <a href={link} target='_blank' rel='noreferrer'><h3>{name}</h3></a>
+                    <div className='card-front'>
+                        <h3>{name}</h3>
                     </div>
-                    <div className='card-content'>
-                        <p className='description'>{description}</p>
-                        {/* <div className='preview'>
-                            <p className='symbol' id='triangle'>&#9660;</p>
-                            <p className='preview-text'>preview</p>
-                        </div> */}
-                        <div className='project-links-wrapper'>
-                            <div className='project-links'>
-                                <a href={props.link} target='_blank' rel='noreferrer'>see it</a> 
-                                <a href={props.github} target='_blank' rel='noreferrer'>see the code</a>
+
+                    {/* <div className='card-back'>
+                        <div className='title-wrapper'>
+                            <a href={link} target='_blank' rel='noreferrer'><h3>{name}</h3></a>
+                        </div>
+                        <div className='card-content'>
+                            <p className='description'>{description}</p>
+                            <div className='preview'>
+                                <p className='symbol' id='triangle'>&#9660;</p>
+                                <p className='preview-text'>preview</p>
+                            </div>
+                            <div className='project-links-wrapper'>
+                                <div className='project-links'>
+                                    <a href={props.link} target='_blank' rel='noreferrer'>see it</a> 
+                                    <a href={props.github} target='_blank' rel='noreferrer'>see the code</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
     )
